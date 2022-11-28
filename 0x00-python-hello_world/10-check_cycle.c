6c9176d5e7ed55fa209f1b5a64ec7cb55c16701a
Diana-ak <dianaambani15@gmail.com>
2022-11-28 08:17:57 -0800

    python hellow

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle
+ * @list: A singly-linked list
+ *
+ * Return: If there is no cycle - 0
+ * 	If there is a cycle - 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *dog, *cat;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	
+	dog = list->next;
+	cat = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (dog == cat)
+			return (1);
+
+		dog = dog->next;
+		cat = cat->next;
+	}
+
+	return (0);
+}
