383d1f330295c503ad013bd3a0884aaea30868bf
Diana-ak <dianaambani15@gmail.com>
2022-11-29 10:33:41 -0800

    commit three

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = [-1]
-print(f"Last digit of {number:d} is {last_digit:d} and is ", end="")
+if number > 0:
+    digit = number % 10
+else:
+    digit = ((number * -1) % 10) * -1
+
+print("Last digit of {:d} is {:d}".format(number, digit), end=" ")
+
 if last_digit > 5:
-    print("greater than 5")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print("0")
+    print("and is 0")
 else:
     print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-     if chr(i) == 'e' or chr(i) == 'q':
+for letter in range(97, 123):
+     if letter == 101 or letter == 113:
          continue
      else:
-         print("{:s}".format(chr(i)), end="")
+         print("{}".format(chr(letter)), end="")
