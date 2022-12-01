d9c141f84e0bbdb3d0be401871673af66106e7ce
Diana-ak <dianaambani15@gmail.com>
2022-12-01 09:26:52 -0800

    commit two

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    if argv_count is 0:
-        print("{:d} arguments.".format(argv_count))
-    elif argv_count is 1:
-        print("{:d} argument:".format(argv_count))
-        print("{:d}: {:s}".format(index, sys.argv[1]))
+    arg_count = len(argv) - 1
+    first_element = argv[0]
+    last_element = argv[argc-1]
+    argv = sys.argv[i]
+    i = 1
+
+    if arg_count == 1:
+        print("{:d} 'argument'".format(arg_count, 'argument'))
     else:
-        print("{:d} arguments:".format(argv_count))
-        while index <= argv_count:
-            print("{:d}: {:s}".format(index, sys.argv[index]))
-            index += 1
+        print("{:d} 'arguments'".format(arg_count, 'arguments'))
+
+    if arg_count => i:
+        print("{:d} {:s}".format(i, 'argv'))
+    else:
+        print("{:d} {:s.}".format(i, 'argv'))
