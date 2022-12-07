a2b6b4f557251d8727fd36558193affc76d730d1
Diana-ak <dianaambani15@gmail.com>
2022-12-07 03:41:04 -0800

    commit one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-   new_list = []
-   for x in my_list:
-       if my_list == search:
-           new_list.append(replace)
-       else:
-           new_list.append(element)
-    return new_list
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
